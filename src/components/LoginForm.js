a4975a6c09a49bc349706ff48fa745a8db3b21f7
Sahil Kansal <kabir@Sahil-kansal.local>
2018-08-01 12:18:13 +0530

    Login Form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,61 +6,6 @@ import { Button, CardSection, Spinner } from './common';
 class LoginForm extends Component{
   state = { email: '', password: '', error: '', loading: false };
 
-// onButtonPress() {
-//   const { email, password } = this.state;
-//   this.setState({ error: '', loading: true });
-//   console.log(email);
-//   this.props.navigation.navigate('LoggedInUser');
-
-//  firebase.auth().signInWithEmailAndPassword(email, password)
-//   .then(this.onLoginSuccess.bind(this))
-//   .catch( () => {
-//     firebase.auth().createUserWithEmailAndPassword(email, password)
-//     .then(this.onLoginSuccess.bind(this))
-//     .catch(this.onLoginFail.bind(this));
-//   });
-
-/*  firebase.database().ref('data').push({
-    email: email, pass: password
-  })*/
- /*console.log('outer');
-  firebase.database().ref('data').once('value',function(snap){
-    console.log(snap.val())
-
-    // snapshot.forEach(function(childSnapshot) {
-    //   var childKey = childSnapshot.key;
-    //   var childData = childSnapshot.val();
-    //   console.log(childData.email);
-    // });
-  })*/
-
-// }
-
-// onLoginFail() {
-//     this.setState( {
-//       error: 'authentication failed',
-//       loading: false
-//     });
-// }
-
-// onLoginSuccess() {
-//   this.setState( {
-//     email: '',
-//     password: '',
-//     loading: 'false',
-//     error: ''
-//   } );
-//   {this.renderButton()}
-// }
-// renderButton(){
-//       // if (this.state.loading == true){
-//       //   return <Spinner size={'small'} />;
-//       // }
-//       return (
-       
-         
-//       );
-//   }
 render(){
     return (
       <View>
